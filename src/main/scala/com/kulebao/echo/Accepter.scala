604d45499067171aeb771f2af46c080094544012
Suo Qin <kpsesuo@gmail.com>
2014-12-02 22:04:00 +0800

    answer link cmd only

--- src/main/scala/com/kulebao/echo/Accepter.scala
+++ src/main/scala/com/kulebao/echo/Accepter.scala
@@ -1,13 +1,7 @@
 package com.kulebao.echo
 
-import java.util.Date
-
 import akka.actor._
-import akka.io.Tcp.Write
-import akka.util.ByteString
 import com.kulebao.handler._
-import org.joda.time.DateTime
-import com.kulebao.db.DB
 
 
 object EchoHandlerProps extends HandlerProps {
@@ -18,20 +12,15 @@ class EchoHandler(connection: ActorRef) extends Handler(connection) {
 
   def received(data: String) = {
     log.debug(s"received: $data")
-    if (false)
-      connection ! Write(reply(data))
     val actor: ActorRef = context.actorOf(Props[DbWriter])
-    data.split("#\\*").foreach(actor ! _)
+    val linkAnswer: ActorRef = context.actorOf(AnswerLinkProps.props(connection))
+    data.split("#\\*").foreach {
+      (cmd) =>
+        actor ! cmd
+        linkAnswer ! cmd
+    }
+
   }
 
-  def reply(input: String): ByteString = {
-    println(input)
-    val prefix = input.split(",").take(2).mkString(",")
 
-    val formattedTime = DateTime.now().toString("HHmmss")
-
-    val output: String = s"$prefix,D1,$formattedTime,60,1#"
-    log.debug(s"reply: $output")
-    ByteString(output)
-  }
 }
---
+++ src/main/scala/com/kulebao/echo/AnswerLink.scala
@@ -0,0 +1,47 @@
+package com.kulebao.echo
+
+import akka.actor.{ActorLogging, ActorRef, Props, UntypedActor}
+import akka.io.Tcp.Write
+import akka.util.ByteString
+import com.kulebao.handler.HandlerProps
+import org.joda.time.DateTime
+
+object AnswerLinkProps extends HandlerProps {
+  def props(connection: ActorRef) = Props(classOf[AnswerLink], connection)
+}
+
+class AnswerLink(connection: ActorRef) extends UntypedActor with ActorLogging {
+
+  val cmdLink = """^\*\w{2},(\w+?),LINK,(\d{6}),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d{6}),(.+?)#$""".r
+
+  @throws(classOf[Exception])
+  override def onReceive(message: Any): Unit = {
+    message match {
+      case data: String =>
+
+        println(s"AnswerLink received: $data")
+        log.debug(s"received: $data")
+        data match {
+          case cmdLink(id, time, gsm, gps, bat, step, turnover, ex1, ex2, date, status) =>
+            log.debug("link cmd, reply D1 cmd")
+            connection ! Write(reply(data))
+          case _ =>
+            log.debug("not a link cmd, don't reply")
+        }
+      case _ =>
+        log.debug(s"not link cmd: $message")
+    }
+  }
+
+
+  def reply(input: String): ByteString = {
+    println(input)
+    val prefix = input.split(",").take(2).mkString(",")
+
+    val formattedTime = DateTime.now().toString("HHmmss")
+
+    val output: String = s"$prefix,D1,$formattedTime,60,1#"
+    log.debug(s"reply: $output")
+    ByteString(output)
+  }
+}
